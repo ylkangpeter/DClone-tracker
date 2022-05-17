e0179b369fce82fdf1a2ee1782be3014f63514a9
kingpeter <ylkangpeter@gmail.com>
2022-05-17 16:47:41 +0800

    add toggle configuration

--- dclone_tracker/background.js
+++ dclone_tracker/background.js
@@ -1,48 +1,65 @@
 try {
-    importScripts("util.js");
+  importScripts("util.js");
 } catch (e) {
-    console.log(e);
+  console.log(e);
 }
 
-let intervalInSeconds = 60;
+let intervalInSeconds = 10;
 // set current alert level to 5. So 5 or 6 will trigger alert.
-let alertLevel = 5;
+// let alertLevel = 2;
 
 
 let jspIntervalInSeconds = 30;
-let keyword='dclone';
+let keyword = 'dclone';
+let LOCAL_KEY = "key";
+let ALERT_LEVEL_KEY = "alert";
+let JSP_TOGGLE_KEY = "jsp";
 
+async function looping() {
 
-function looping() {
+  console.log("[background] d2 start fetching data every " + intervalInSeconds + " seconds.")
+  var result = await fetch('https://diablo2.io/dclone_api.php')
 
-  console.log("[background] d2 start fetching data every "+ intervalInSeconds + " seconds.")
-  fetch('https://diablo2.io/dclone_api.php')
-    .then(result => {
-      if(result.status === 200){
-        return result.json();
-      }
-        console.log("[background] something wrong when connecting. status code: " + result.status);
-      })
-    .then(result => {
-        console.log("[background] " + result);
-        var results = jsonParser(result);
-        var msg = ""; 
-        for (let result of results) {
-          if(result.progress>=alertLevel){
+  // console.log("[background] " + result);
 
-            chrome.action.setBadgeText ( { text: 'NEW' } );
-            chrome.action.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
-            
-            msg = "Prime evil is coming!";
-            // may add some details in msg later.
-            toPopup(msg);
-            break;
-          }
+  if (result.status === 200) {
+    var text = await result.json();
+    var results = jsonParser(text);
+    var msg = "";
+
+    var alertConfig = await chrome.storage.local.get(ALERT_LEVEL_KEY);
+    var alertLevel = alertConfig[ALERT_LEVEL_KEY] || 4;
+
+    console.log(`[background]-alert config- level: ${alertLevel}`);
+
+    for (let result of results) {
+      if (result.progress >= alertLevel) {
+        var key = result.ladder + "_" + result.hc + "_" + result.region;
+        console.log(`[background]-${key}`)
+
+        var data = await chrome.storage.local.get(LOCAL_KEY);
+        var stats = data[LOCAL_KEY] || "{}";
+        console.log(`[background]-${stats}`)
+        if (stats[key] != true) {
+          console.log("-----" + key + "------triggered, but not toggled!")
+        } else {
+          chrome.action.setBadgeText({
+            text: 'NEW'
+          });
+          chrome.action.setBadgeBackgroundColor({
+            color: [255, 0, 0, 255]
+          });
+
+          msg = "Prime evil is coming!";
+          // may add some details in msg later.
+          toPopup(msg);
+          return;
         }
-      })
-    .catch((error) => {
-      console.log(error)
-      });
+      }
+    }
+  } else {
+    console.log(`[background]-error-${result}`);
+  }
 }
 
 function toPopup(msg) {
@@ -57,42 +74,41 @@ function toPopup(msg) {
   chrome.notifications.create(options, callback);
 }
 
-function callback(){
+function callback() {
   console.log("[background] send notifications.");
 
 }
+
 function optionChecker() {
   // reset state according to option parameters.
 }
 
-function d2jspTracker(){
-  console.log("[background] jsp start fetching data every "+ jspIntervalInSeconds + " seconds.")
-  fetch('https://forums.d2jsp.org/forum.php?f=271&t=5')
-    .then(result => {
-      if(result.status === 200){
-        return result.text();
-      }
-        console.log("[background] something wrong when connecting. status code: " + result.status);
-      })
-    .then(result => {
-        console.log("[background] " + result);
-        
-        if(result.toLowerCase().indexOf(keyword)>=0){
-          console.log("d2jsp talking about dclone");
-          chrome.action.setBadgeText ( { text: 'jsp' } );
-          chrome.action.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
-            
-          msg = "jsp dclone topics.";
-          // may add some details in msg later.
-          toPopup(msg);
-        }
-      })
-    .catch((error) => {
-      console.log(error)
+async function d2jspTracker() {
+  console.log("[background] jsp start fetching data every " + jspIntervalInSeconds + " seconds.")
+  var jspConfig = await chrome.storage.local.get(JSP_TOGGLE_KEY);
+  var jspToggle = jspConfig[JSP_TOGGLE_KEY] || true;
+  console.log("[background]-jsp config- toggle jsp:${jspToggle}");
+  if (jspToggle) {
+    var result = await fetch('https://forums.d2jsp.org/forum.php?f=271&t=5')
+    console.log("[background] " + result);
+    var v = await result.text()
+    if (v.toLowerCase().indexOf(keyword) >= 0) {
+      console.log("d2jsp talking about dclone");
+      chrome.action.setBadgeText({
+        text: 'jsp'
       });
+      chrome.action.setBadgeBackgroundColor({
+        color: [255, 0, 0, 255]
+      });
+
+      msg = "jsp dclone topics.";
+      // may add some details in msg later.
+      toPopup(msg);
+    }
+  }
 }
 
-setInterval(looping, intervalInSeconds*1000);
-setInterval(d2jspTracker, jspIntervalInSeconds*1000);
+setInterval(looping, intervalInSeconds * 1000);
+setInterval(d2jspTracker, jspIntervalInSeconds * 1000);
 
 optionChecker()
--- dclone_tracker/popup.js
+++ dclone_tracker/popup.js
@@ -1,64 +1,94 @@
 // Initialize butotn with users's prefered color
 let progress = document.getElementById("dCloneProgress");
-let regionDict = {1:"Americas",2:"Europe",3:"Asia"};
+let jsp = document.getElementById("toggleJsp");
+let alertLevel = document.getElementById("alertLevel");
+
+let ALERT_LEVEL_KEY = "alert";
+let JSP_TOGGLE_KEY = "jsp";
+
+
+let regionDict = {
+  1: "Americas",
+  2: "Europe",
+  3: "Asia"
+};
 let totalDCloneProgress = 6;
+let LOCAL_KEY = "key";
 
-function tracking(){
-
-  chrome.action.setBadgeText ( { text: '' } );
-  
-  console.log("loading..")
-  fetch('https://diablo2.io/dclone_api.php').then(result => {
-      if(result.status === 200){
-        return result.json();
-      }
-       console.log("[popup] something wrong when connecting. status code: " + result.status);
-     }).then(result =>{
-        console.log(result);
-        var rows = jsonParser(result);
-
-        var renderHtml ="<table class='styled-table'><theadh><td>No.</td><td>Progress</td><td>ladder</td><td>Hardcore</td><td>Region</td><td>Update Time</td></thead>"
-        for (var i=0;i<rows.length;i++) {
-          renderHtml+="<tr><td>"
-          renderHtml+=i+1;
-          renderHtml+="</td>"    
-
-          renderHtml+="<td>"
-          renderHtml+=genProgressBar(rows[i].progress);
-          renderHtml+="</td>"        
-
-          renderHtml+="<td>"
-          renderHtml+=(rows[i].ladder==1?"ladder":"non-ladder");
-          renderHtml+="</td>"        
-
-          renderHtml+="<td>"
-          renderHtml+=(rows[i].hc==1?"Hardcore":"Softcore");
-          renderHtml+="</td>"   
-
-          renderHtml+="<td>"
-          renderHtml+=regionDict[rows[i].region];
-          renderHtml+="</td>"        
-
-          renderHtml+="<td>"
-          renderHtml+=getTimestamp(rows[i].timestamped);
-          renderHtml+="</td></tr>"            
-        }
-        renderHtml+="</table>";
-
-        progress.innerHTML=renderHtml;
-  }).catch((error) => {
-  console.log(error)
-  });
+function changeAlert(inx) {
+  console.log(inx);
 }
 
-function genProgressBar(progress){
+async function tracking() {
+
+  chrome.action.setBadgeText({
+    text: ''
+  });
+  // await new Promise(r => setTimeout(r, 4000));
+  console.log("loading..")
+  var result = await fetch('https://diablo2.io/dclone_api.php')
+  console.log(result)
+
+  if (result.status === 200) {
+
+    // console.log(result);
+    var text = await result.json();
+    var rows = jsonParser(text);
+
+    var renderHtml = "<table class='styled-table'><theadh><td>No.</td><td>Toggle alert</td><td>Progress</td><td>ladder</td><td>Hardcore</td><td>Region</td><td>Update Time</td></thead>"
+    for (var i = 0; i < rows.length; i++) {
+      var key = rows[i].ladder + "_" + rows[i].hc + "_" + rows[i].region;
+      var checked = await getStatus(key);
+
+      renderHtml += "<tr><td>"
+      renderHtml += i + 1;
+      renderHtml += "</td>"
+      console.log("checked: " + checked);
+
+      var checkV = checked ? "checked" : "";
+      console.log("checkV: " + checkV);
+      renderHtml += "<td>"
+      renderHtml += "<section class='model-1'><div class='checkbox'><input type='checkbox' id='" + i + "' data-inner-attr='" + key + "' " + checkV + "/><label></label></div></section>";
+      renderHtml += "</td>"
+
+      renderHtml += "<td>"
+      renderHtml += genProgressBar(rows[i].progress);
+      renderHtml += "</td>"
+
+      renderHtml += "<td>"
+      renderHtml += (rows[i].ladder == 1 ? "ladder" : "non-ladder");
+      renderHtml += "</td>"
+
+      renderHtml += "<td>"
+      renderHtml += (rows[i].hc == 1 ? "Hardcore" : "Softcore");
+      renderHtml += "</td>"
+
+      renderHtml += "<td>"
+      renderHtml += regionDict[rows[i].region];
+      renderHtml += "</td>"
+
+      renderHtml += "<td>"
+      renderHtml += getTimestamp(rows[i].timestamped);
+      renderHtml += "</td></tr>"
+    }
+    renderHtml += "</table>";
+
+    progress.innerHTML = renderHtml;
+
+    addListener();
+  } else {
+    console.log(result);
+  }
+}
+
+function genProgressBar(progress) {
   var result = "<div class='container'><div class='progress-segment'>";
-  var i=0;
-  for(;i<progress;i++){
+  var i = 0;
+  for (; i < progress; i++) {
     result += "<div class='item my-common'></div>"
   }
 
-  for(;i<totalDCloneProgress;i++){
+  for (; i < totalDCloneProgress; i++) {
     result += "<div class='item'></div>"
   }
 
@@ -66,4 +96,55 @@ function genProgressBar(progress){
   return result;
 }
 
+async function initConfig(){
+  // jsp
+  var jspConfig = await chrome.storage.local.get(JSP_TOGGLE_KEY);
+  var cur = jspConfig[JSP_TOGGLE_KEY]||true
+  jsp.checked=cur;
+
+  // alert level
+  var lv = await chrome.storage.local.get(ALERT_LEVEL_KEY);
+  var cur = lv[ALERT_LEVEL_KEY]||4
+  alertLevel.value=cur;
+}
+
+function addListener() {
+  progress.addEventListener('click', async (e) => {
+    console.log(e);
+    if (e.target.tagName == 'INPUT') {
+      var newStat = e.target.checked;
+      var stats = await chrome.storage.local.get(LOCAL_KEY);
+      stats[LOCAL_KEY][e.target.dataset.innerAttr] = newStat;
+      chrome.storage.local.set(stats);
+    }
+  }, false)
+
+  alertLevel.addEventListener('click', async (e) => {
+    console.log(e);
+    var newLevel = e.target.value;
+    var lv = await chrome.storage.local.get(ALERT_LEVEL_KEY);
+    var cur = lv[ALERT_LEVEL_KEY]||4
+    console.log(`alert level changed from ${cur} to ${newLevel}`)
+    lv[ALERT_LEVEL_KEY]=newLevel;
+    chrome.storage.local.set(lv);
+  }, false)
+
+  jsp.addEventListener('click', async (e) => {
+    console.log(e);    
+    var checked = e.target.checked;
+    var jspConfig = await chrome.storage.local.get(JSP_TOGGLE_KEY);
+    var cur = jspConfig[JSP_TOGGLE_KEY]||true
+    console.log(`jspConfig changed from ${cur} to ${checked}`)
+    jspConfig[JSP_TOGGLE_KEY]=checked;
+    chrome.storage.local.set(jspConfig);
+  }, false)
+}
+
+async function getStatus(key) {
+  var stats = await chrome.storage.local.get(LOCAL_KEY);
+  console.log(stats[key] || false)
+  return stats[LOCAL_KEY][key] || false;
+}
+
+initConfig();
 tracking();
